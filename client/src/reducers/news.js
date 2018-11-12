68f3321eb25c2b491d62205f4d4f9a89b1a25ee1
Luis Sanchez Castellanos <luis.sanchez@schibsted.com>
2018-11-12 16:52:45 +0000

    Redux added to the project

---
+++ client/src/reducers/news.js
@@ -0,0 +1,12 @@
+const initialState = {
+    list: []
+};
+
+const news = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default news;
